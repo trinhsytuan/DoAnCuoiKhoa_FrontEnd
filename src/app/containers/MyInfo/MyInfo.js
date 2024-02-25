fd368163c2ebbd521e0df88ecd5217d4616e1240
Tuan <trinhsytuan@gmail.com>
2024-02-25 22:25:03 +0700

    fix bg

--- src/app/containers/MyInfo/MyInfo.js
+++ src/app/containers/MyInfo/MyInfo.js
@@ -1,43 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Button, Col, Form, Input, Modal, Row, Table, Tabs } from "antd";
-import {
-  DeleteOutlined,
-  EditOutlined,
-  PlusOutlined,
-  SaveFilled,
-} from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, PlusOutlined, SaveFilled } from "@ant-design/icons";
 
 import CustomSkeleton from "@components/CustomSkeleton";
 import DropzoneImage from "@components/DropzoneImage";
 import "./MyInfo.scss";
 import { CONSTANTS, LOAI_TAI_KHOAN, RULES, VI_ROLE_SYSTEM } from "@constants";
-import {
-  cloneObj,
-  formatSTT,
-  toast,
-  validateSpaceNull,
-} from "@app/common/functionCommons";
+import { cloneObj, formatSTT, toast, validateSpaceNull } from "@app/common/functionCommons";
 import { requestChangePassword } from "@app/services/User";
 
 import * as user from "@app/store/ducks/user.duck";
 import * as app from "@app/store/ducks/app.duck";
-import {
-  createCategory,
-  deleteCategory,
-  getAllCategory,
-  updateCategory,
-} from "@app/services/Category";
+import { createCategory, deleteCategory, getAllCategory, updateCategory } from "@app/services/Category";
 import DialogDeleteConfim from "@components/DialogDeleteConfim/DialogDeleteConfim";
+import BaseContent from "@components/BaseContent";
 
-function MyInfo({
-  myInfo,
-  isLoading,
-  roleList,
-  requestChuyenMuc,
-  chuyenMuc,
-  ...props
-}) {
+function MyInfo({ myInfo, isLoading, roleList, requestChuyenMuc, chuyenMuc, ...props }) {
   const [formInfo] = Form.useForm();
   const [formChangePassword] = Form.useForm();
   const [avatarTemp, setAvatarTemp] = useState(null);
@@ -121,11 +100,7 @@ function MyInfo({
                 handleEdit(value);
               }}
             ></Button>
-            <Button
-              icon={<DeleteOutlined />}
-              style={{ border: 0 }}
-              onClick={() => deleteChuyenMuc(value)}
-            ></Button>
+            <Button icon={<DeleteOutlined />} style={{ border: 0 }} onClick={() => deleteChuyenMuc(value)}></Button>
           </div>
         );
       },
@@ -176,151 +151,75 @@ function MyInfo({
   };
   return (
     <div>
-      <Tabs size="small">
-        <Tabs.TabPane tab="Thông tin cá nhân" key="1">
-          <Row>
-            <Col sm={18}>
-              <Form
-                form={formInfo}
-                id="form-info"
-                autoComplete="off"
-                onFinish={handleUpdateMyInfo}
-              >
-                <Row gutter={15}>
-                  <CustomSkeleton
-                    size="default"
-                    label="Email"
-                    name="username"
-                    type={CONSTANTS.TEXT}
-                    labelCol={{ xs: 8 }}
-                    layoutCol={{ xs: 24 }}
-                    disabled={true}
-                    showInputLabel={
-                      myInfo.loaiTaiKhoan ===
-                      LOAI_TAI_KHOAN.TAI_KHOAN_HRMS.value
-                    }
-                    form={formInfo}
-                  />
-                  <CustomSkeleton
-                    size="default"
-                    label="Vai trò"
-                    name="role"
-                    type={CONSTANTS.TEXT}
-                    labelCol={{ xs: 8 }}
-                    layoutCol={{ xs: 24 }}
-                    disabled={true}
-                    form={formInfo}
-                  />
-                  <CustomSkeleton
-                    size="default"
-                    label="Khoá công khai"
-                    name="publicKey"
-                    type={CONSTANTS.TEXT}
-                    labelCol={{ xs: 8 }}
-                    layoutCol={{ xs: 24 }}
-                    disabled={true}
-                  />
-                  <CustomSkeleton
-                    size="default"
-                    label="Khoá bí mật"
-                    name="privateKey"
-                    type={CONSTANTS.TEXT}
-                    labelCol={{ xs: 8 }}
-                    layoutCol={{ xs: 24 }}
-                    disabled={true}
-                    helpInline={false}
-                  />
-                </Row>
-              </Form>
-            </Col>
-            <Col sm={6}>
-              <div className="attach-image">
-                <div className="attach-image__title">Ảnh đại diện</div>
-                <div className="attach-image__img">
-                  <DropzoneImage
-                    width={38 * 5}
-                    height={38 * 5}
-                    imgUrl={myInfo.avatar}
-                    stateRerender={myInfo.avatar}
-                    handleDrop={handleSelectAvatar}
-                  />
+      <BaseContent>
+        <Tabs size="small">
+          <Tabs.TabPane tab="Thông tin cá nhân" key="1">
+            <Row>
+              <Col sm={18}>
+                <Form form={formInfo} id="form-info" autoComplete="off" onFinish={handleUpdateMyInfo}>
+                  <Row gutter={15}>
+                    <CustomSkeleton
+                      size="default"
+                      label="Email"
+                      name="username"
+                      type={CONSTANTS.TEXT}
+                      labelCol={{ xs: 8 }}
+                      layoutCol={{ xs: 24 }}
+                      disabled={true}
+                      showInputLabel={myInfo.loaiTaiKhoan === LOAI_TAI_KHOAN.TAI_KHOAN_HRMS.value}
+                      form={formInfo}
+                    />
+                    <CustomSkeleton
+                      size="default"
+                      label="Vai trò"
+                      name="role"
+                      type={CONSTANTS.TEXT}
+                      labelCol={{ xs: 8 }}
+                      layoutCol={{ xs: 24 }}
+                      disabled={true}
+                      form={formInfo}
+                    />
+                    <CustomSkeleton
+                      size="default"
+                      label="Khoá công khai"
+                      name="publicKey"
+                      type={CONSTANTS.TEXT}
+                      labelCol={{ xs: 8 }}
+                      layoutCol={{ xs: 24 }}
+                      disabled={true}
+                    />
+                    <CustomSkeleton
+                      size="default"
+                      label="Khoá bí mật"
+                      name="privateKey"
+                      type={CONSTANTS.TEXT}
+                      labelCol={{ xs: 8 }}
+                      layoutCol={{ xs: 24 }}
+                      disabled={true}
+                      helpInline={false}
+                    />
+                  </Row>
+                </Form>
+              </Col>
+              <Col sm={6}>
+                <div className="attach-image">
+                  <div className="attach-image__title">Ảnh đại diện</div>
+                  <div className="attach-image__img">
+                    <DropzoneImage
+                      width={38 * 5}
+                      height={38 * 5}
+                      imgUrl={myInfo.avatar}
+                      stateRerender={myInfo.avatar}
+                      handleDrop={handleSelectAvatar}
+                    />
+                  </div>
                 </div>
-              </div>
-            </Col>
-
-            <Col xs={24}>
-              <Button
-                htmlType="submit"
-                form="form-info"
-                type="primary"
-                className="float-right"
-                icon={<SaveFilled />}
-                disabled={isLoading}
-              >
-                Lưu
-              </Button>
-            </Col>
-          </Row>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Đổi mật khẩu" key="2">
-          <Form
-            form={formChangePassword}
-            autoComplete="off"
-            onFinish={handleChangePassword}
-            onValuesChange={onValuesChange}
-          >
-            <Row gutter={15}>
-              <CustomSkeleton
-                size="default"
-                label="Mật khẩu cũ"
-                name="oldPassword"
-                type={CONSTANTS.PASSWORD}
-                rules={[RULES.REQUIRED]}
-                layoutCol={{ xs: 24 }}
-                labelCol={{ xs: 8 }}
-                disabled={isLoading}
-                helpInline={false}
-              />
-              <CustomSkeleton
-                size="default"
-                label="Mật khẩu mới"
-                name="newPassword"
-                type={CONSTANTS.PASSWORD}
-                layoutCol={{ xs: 24 }}
-                labelCol={{ xs: 8 }}
-                rules={[RULES.REQUIRED, RULES.PASSWORD_FORMAT]}
-                disabled={isLoading}
-                helpInline={false}
-              />
-              <CustomSkeleton
-                size="default"
-                label="Xác nhận mật khẩu mới"
-                name="confirmPassword"
-                layoutCol={{ xs: 24 }}
-                labelCol={{ xs: 8 }}
-                type={CONSTANTS.PASSWORD}
-                helpInline={false}
-                rules={[
-                  RULES.REQUIRED,
-                  ({ getFieldValue }) => ({
-                    validator(rule, confirmPassword) {
-                      if (
-                        confirmPassword &&
-                        getFieldValue("newPassword") !== confirmPassword
-                      ) {
-                        return Promise.reject("Mật khẩu mới không trùng khớp");
-                      }
-                      return Promise.resolve();
-                    },
-                  }),
-                ]}
-                disabled={isLoading}
-                form={formInfo}
-              />
+              </Col>
 
               <Col xs={24}>
                 <Button
                   htmlType="submit"
+                  form="form-info"
                   type="primary"
                   className="float-right"
                   icon={<SaveFilled />}
@@ -330,65 +229,114 @@ function MyInfo({
                 </Button>
               </Col>
             </Row>
-          </Form>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Chuyên mục" key="3">
-          <div className="chuyenmuc-myinfo-container">
-            <div className="btn-actions">
-              <Button
-                type="primary"
-                icon={<PlusOutlined />}
-                onClick={cancelThemChuyenMuc}
-              >
-                Thêm chuyên mục
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Đổi mật khẩu" key="2">
+            <Form
+              form={formChangePassword}
+              autoComplete="off"
+              onFinish={handleChangePassword}
+              onValuesChange={onValuesChange}
+            >
+              <Row gutter={15}>
+                <CustomSkeleton
+                  size="default"
+                  label="Mật khẩu cũ"
+                  name="oldPassword"
+                  type={CONSTANTS.PASSWORD}
+                  rules={[RULES.REQUIRED]}
+                  layoutCol={{ xs: 24 }}
+                  labelCol={{ xs: 8 }}
+                  disabled={isLoading}
+                  helpInline={false}
+                />
+                <CustomSkeleton
+                  size="default"
+                  label="Mật khẩu mới"
+                  name="newPassword"
+                  type={CONSTANTS.PASSWORD}
+                  layoutCol={{ xs: 24 }}
+                  labelCol={{ xs: 8 }}
+                  rules={[RULES.REQUIRED, RULES.PASSWORD_FORMAT]}
+                  disabled={isLoading}
+                  helpInline={false}
+                />
+                <CustomSkeleton
+                  size="default"
+                  label="Xác nhận mật khẩu mới"
+                  name="confirmPassword"
+                  layoutCol={{ xs: 24 }}
+                  labelCol={{ xs: 8 }}
+                  type={CONSTANTS.PASSWORD}
+                  helpInline={false}
+                  rules={[
+                    RULES.REQUIRED,
+                    ({ getFieldValue }) => ({
+                      validator(rule, confirmPassword) {
+                        if (confirmPassword && getFieldValue("newPassword") !== confirmPassword) {
+                          return Promise.reject("Mật khẩu mới không trùng khớp");
+                        }
+                        return Promise.resolve();
+                      },
+                    }),
+                  ]}
+                  disabled={isLoading}
+                  form={formInfo}
+                />
+
+                <Col xs={24}>
+                  <Button
+                    htmlType="submit"
+                    type="primary"
+                    className="float-right"
+                    icon={<SaveFilled />}
+                    disabled={isLoading}
+                  >
+                    Lưu
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Chuyên mục" key="3">
+            <div className="chuyenmuc-myinfo-container">
+              <div className="btn-actions">
+                <Button type="primary" icon={<PlusOutlined />} onClick={cancelThemChuyenMuc}>
+                  Thêm chuyên mục
+                </Button>
+              </div>
+              <div className="table-instruction">
+                <Table columns={columnChuDe} dataSource={chuyenMuc} pagination={false}></Table>
+              </div>
+            </div>
+          </Tabs.TabPane>
+        </Tabs>
+        <Modal
+          visible={visibleThemChuyenMuc}
+          onCancel={cancelThemChuyenMuc}
+          title="Thêm chuyên mục mới"
+          footer={null}
+          width={700}
+        >
+          <Form
+            form={form}
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 18 }}
+            onFinish={createChuyenMuc}
+            className="formCreateChuyenMuc"
+          >
+            <Form.Item name="keyId" hidden={true}></Form.Item>
+            <Form.Item name="name" label="Tên chuyên mục" rules={[RULES.REQUIRED, { validator: validateSpaceNull }]}>
+              <Input placeholder="Vui lòng nhập tên chuyên mục"></Input>
+            </Form.Item>
+            <div className="btn-submit-actions">
+              <Button type="primary" htmlType="submit">
+                {form.getFieldValue("keyId") ? "Lưu thông tin" : "Thêm chuyên mục"}
               </Button>
             </div>
-            <div className="table-instruction">
-              <Table
-                columns={columnChuDe}
-                dataSource={chuyenMuc}
-                pagination={false}
-              ></Table>
-            </div>
-          </div>
-        </Tabs.TabPane>
-      </Tabs>
-      <Modal
-        visible={visibleThemChuyenMuc}
-        onCancel={cancelThemChuyenMuc}
-        title="Thêm chuyên mục mới"
-        footer={null}
-        width={700}
-      >
-        <Form
-          form={form}
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 18 }}
-          onFinish={createChuyenMuc}
-          className="formCreateChuyenMuc"
-        >
-          <Form.Item name="keyId" hidden={true}></Form.Item>
-          <Form.Item
-            name="name"
-            label="Tên chuyên mục"
-            rules={[RULES.REQUIRED, { validator: validateSpaceNull }]}
-          >
-            <Input placeholder="Vui lòng nhập tên chuyên mục"></Input>
-          </Form.Item>
-          <div className="btn-submit-actions">
-            <Button type="primary" htmlType="submit">
-              {form.getFieldValue("keyId")
-                ? "Lưu thông tin"
-                : "Thêm chuyên mục"}
-            </Button>
-          </div>
-        </Form>
-      </Modal>
-      <DialogDeleteConfim
-        visible={visibleDelete}
-        onCancel={handleConfim}
-        onOK={handleDeleteCM}
-      />
+          </Form>
+        </Modal>
+        <DialogDeleteConfim visible={visibleDelete} onCancel={handleConfim} onOK={handleDeleteCM} />
+      </BaseContent>
     </div>
   );
 }
@@ -400,6 +348,4 @@ function mapStateToProps(store) {
   return { isLoading, myInfo, requestChuyenMuc, chuyenMuc };
 }
 
-export default connect(mapStateToProps, { ...app.actions, ...user.actions })(
-  MyInfo
-);
+export default connect(mapStateToProps, { ...app.actions, ...user.actions })(MyInfo);
