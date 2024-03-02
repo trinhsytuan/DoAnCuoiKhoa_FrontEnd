72a997ab90e6377b72c102bdb8735d7dfc2d66fc
Tuan <trinhsytuan@gmail.com>
2024-03-02 21:59:06 +0700

    Fix file not show

--- src/app/components/FileAction/FileAction.js
+++ src/app/components/FileAction/FileAction.js
@@ -8,34 +8,36 @@ import DELETE from "@assets/images/icon/delete.svg";
 import DOWNLOAD from "@assets/images/icon/download.svg";
 import "./FileAction.scss";
 import { EditOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
 
-function FileAction({ className }) {
+function FileAction({ className, userOwn, myInfo }) {
   const handleMoveFile = async () => {};
   const handleShareFile = async () => {};
   const handleRenameFile = async () => {};
   const handleDeleteFile = async () => {};
-  const handleDownload = async() => {
-
-  }
+  const handleDownload = async () => {};
 
   const menu = (
     <Menu>
-      <Menu.Item key="DOWNLOAD" onClick={handleDownload} icon={<img src={DOWNLOAD}/>}>
+      <Menu.Item key="DOWNLOAD" onClick={handleDownload} icon={<img src={DOWNLOAD} />}>
         Tải về
       </Menu.Item>
-      <Menu.Item key="COPY" onClick={handleMoveFile} icon={<EditOutlined/>}>
-        Đổi chuyên mục
-      </Menu.Item>
-      <Menu.Item key="SHARE" onClick={handleShareFile} icon={<img src={SHARE}/>}>
-        Chia sẻ
-      </Menu.Item>
-      <Menu.Item key="RENAME" onClick={handleRenameFile} icon={<img src={EDIT}/>}>
-        Đổi tên
-      </Menu.Item>
-      <Menu.Item key="DELETE" onClick={handleDeleteFile} icon={<img src={DELETE}/>}>
-        Xoá
-      </Menu.Item>
-      
+      {userOwn === myInfo?._id && (
+        <>
+          <Menu.Item key="COPY" onClick={handleMoveFile} icon={<EditOutlined />}>
+            Đổi chuyên mục
+          </Menu.Item>
+          <Menu.Item key="SHARE" onClick={handleShareFile} icon={<img src={SHARE} />}>
+            Chia sẻ
+          </Menu.Item>
+          <Menu.Item key="RENAME" onClick={handleRenameFile} icon={<img src={EDIT} />}>
+            Đổi tên
+          </Menu.Item>
+          <Menu.Item key="DELETE" onClick={handleDeleteFile} icon={<img src={DELETE} />}>
+            Xoá
+          </Menu.Item>
+        </>
+      )}
     </Menu>
   );
 
@@ -45,5 +47,8 @@ function FileAction({ className }) {
     </Dropdown>
   );
 }
-
-export default FileAction;
+function mapStateToProps(store) {
+  const { myInfo } = store.user;
+  return { myInfo };
+}
+export default connect(mapStateToProps)(FileAction);
--- src/app/containers/ChuyenMuc/ChuyenMuc.js
+++ src/app/containers/ChuyenMuc/ChuyenMuc.js
@@ -7,39 +7,22 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { getFile } from "@app/services/FileControl";
 import NoData from "@components/NoData/NoData";
 import ICON_FILE from "@assets/images/icon/icon-file.svg";
-import { formatTimeDate } from "@app/common/functionCommons";
+import { convertFileName, formatTimeDate, toast } from "@app/common/functionCommons";
 import FileAction from "@components/FileAction/FileAction";
-import { Button } from "antd";
+import { Button, Upload } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import { API } from "@api";
 import axios from "axios";
+import { CONSTANTS } from "@constants";
 
 function ChuyenMuc({ isLoading, myInfo }) {
-  const fileRef = useRef(null);
-
   const [filter, setFilter] = useState({
     share: null,
     category: null,
   });
-
+  
   const [dataFile, setDataFile] = useState([]);
   const [fileNameSearch, setFilenameSearch] = useState(null);
-  const [precentageUpload, setPrecentageUpload] = useState([]);
-  const handleChangeFileUpload = (idFile, precentage) => {
-    const newData = precentageUpload.map((res, index) => {
-      if (res?.id === idFile) return { ...res, precent: precentage };
-      return { ...res };
-    });
-    setPrecentageUpload(newData);
-  };
-  const handleCreateFileUpload = (fileName, precen = 0) => {
-    const idFile = new Date();
-    const newData = { id: idFile, fileName, precen };
-    if (precentageUpload.length === 0) setPrecentageUpload([{ id: idFile, fileName, precen }]);
-    else setPrecentageUpload([...precentageUpload, ...newData]);
-    return idFile;
-  };
-
   useEffect(() => {
     if (myInfo) {
       getAPI();
@@ -52,38 +35,47 @@ function ChuyenMuc({ isLoading, myInfo }) {
     let share = null;
     if (id === "share-with-me") share = myInfo?._id;
     id = id.length == 24 ? id : null;
-
+    setFilter({
+      share,
+      category: id,
+    });
     const response = await getFile(id, fileNameSearch, share);
     if (response) {
       setDataFile(response);
     }
   };
-  const handleUploadFile = async (file) => {
-    const selectedFile = file.target.files[0];
-    const formData = new FormData();
-    formData.append("file", selectedFile);
-    formData.append("category", filter?.category);
-    const idFile = await handleCreateFileUpload(selectedFile?.name);
-    const response = await axios.post(API.UPLOAD_FILE, formData, {
-      onUploadProgress: async(progressEvent) => {
-        const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-        console.log(percentCompleted);
-        await handleChangeFileUpload(idFile, percentCompleted);
-      },
-    });
+  const customRequest = async ({ file, onSuccess, onError, onProgress }) => {
+    try {
+      const formData = new FormData();
+      const fileNameOrgin = convertFileName(file?.name);
+      formData.append("file", file);
+      formData.append("category", filter?.category);
+      formData.append("fileName", fileNameOrgin);
+      const response = await axios.post(API.UPLOAD_FILE, formData, {
+        onUploadProgress: (progressEvent) => {
+          const uploadPercent = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+          onProgress({ percent: uploadPercent });
+        },
+      });
+      onSuccess();
+      toast(CONSTANTS.SUCCESS, "File đã được tải lên thành công");
+      setDataFile([response.data, ...dataFile])
+    } catch (error) {
+      toast(CONSTANTS.ERROR, "File tải lên có lỗi");
+      onError(error);
+    }
   };
-  const handleClickUploadFile = () => {
-    fileRef.current.click();
-  };
-  console.log(precentageUpload);
   return (
     <div className="chuyen-muc-container">
-      <div className="btn-upload-file-chuyen-muc">
-        <Button type="primary" icon={<UploadOutlined />} onClick={handleClickUploadFile}>
-          Tải file mới
-        </Button>
-        <input type="file" style={{ display: "none" }} ref={fileRef} onChange={handleUploadFile} />
-      </div>
+      {!filter?.share && (
+        <div className="btn-upload-file-chuyen-muc">
+          <Upload customRequest={customRequest} defaultFileList={[]} showUploadList={{ showProgress: true }}>
+            <Button icon={<UploadOutlined />} type="primary">
+              Tải file lên
+            </Button>
+          </Upload>
+        </div>
+      )}
       <Loading active={isLoading}>
         {!dataFile ||
           (!dataFile.length && (
@@ -104,7 +96,7 @@ function ChuyenMuc({ isLoading, myInfo }) {
                     <span className="file-item__time">{formatTimeDate(res?.createdAt)}</span>
                   </div>
                   <div className="file-item__actions">
-                    <FileAction className={"menu-file-share-actions"} idFile={res?._id} />
+                    <FileAction className={"menu-file-share-actions"} idFile={res?._id} userOwn={res?.userOwn} />
                   </div>
                 </div>
               );
